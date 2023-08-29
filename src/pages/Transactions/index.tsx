9766f8663d2a5053d46c890c57d050e42baa0f5f
Aldinei <aldinei.franco@unimedprudente.com.br>
2023-08-29 09:37:45 -0300

    feature(components): Add Tabela de Transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from './styles';
 
 export function Transactions() {
   return (
@@ -7,6 +8,52 @@ export function Transactions() {
       <Header />
       <Summary />
 
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  R$ -12.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Aluguel do apartamento</td>
+              <td>
+                <PriceHighLight variant="outcome">
+                  R$ -5.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Computador</td>
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 6.000,00
+                </PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
 
     </div>
   )
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background-color: ${({ theme }) =>
+      theme["gray-700"]};
+    border-radius: 4px;
+  }
+`;
+
+interface PriceHighLightProps {
+  variant: "income" | "outcome";
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+  color: ${({ variant, theme }) =>
+    variant === "income"
+      ? theme["green-300"]
+      : theme["red-300"]};
+`;
